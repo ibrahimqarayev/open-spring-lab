e970c55a2c8c6969871593565b3b768e86c8864f
G GIRI <yadavgirish406@gmail.com>
2025-12-25 22:40:32 +0530

    feat: add service layer for product entity (#10)

---
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/product/ProductService.java
@@ -0,0 +1,16 @@
+package com.openspringlab.ecommerce.product;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
