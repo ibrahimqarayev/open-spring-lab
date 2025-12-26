90edf837ab6e017cb76c18ed55566864be4979c2
Shubham-150-k <kumbhareshubham33@gmail.com>
2025-12-26 20:40:31 +0530

    Add ProductController with POST /products API

---
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.openspringlab.ecommerce.controller;
+
+import com.openspringlab.ecommerce.product.Product;
+import com.openspringlab.ecommerce.product.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+
+        Product savedProduct = productService.saveProduct(product);
+
+        return new  ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+
+}
