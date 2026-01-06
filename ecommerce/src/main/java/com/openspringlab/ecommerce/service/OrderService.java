b43480b0e3cc69f754a686c95a9f386f1e29e986
İbrahim Qarayev <qarayevibrahim7@mail.ru>
2026-01-06 23:11:57 +0400

    Feature: add order service (#48)

---
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.openspringlab.ecommerce.service;
+
+import com.openspringlab.ecommerce.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+}
