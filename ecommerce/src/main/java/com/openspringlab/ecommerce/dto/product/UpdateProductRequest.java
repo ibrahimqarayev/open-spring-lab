575062a4783901ac4073e515e382c640588de5d6
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-01-10 14:55:20 +0400

    feat: update UpdateProductRequest

--- ecommerce/src/main/java/com/openspringlab/ecommerce/dto/product/UpdateProductRequest.java
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/dto/product/UpdateProductRequest.java
@@ -1,28 +1,16 @@
-package com.openspringlab.ecommerce.dto.request;
+package com.openspringlab.ecommerce.dto.product;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class UpdateProductRequest {
-
     private String name;
-
     private String description;
-
     private BigDecimal price;
-
     private String sku;
-
     private Integer stockQuantity;
-
-    private Boolean active;
-
 }
