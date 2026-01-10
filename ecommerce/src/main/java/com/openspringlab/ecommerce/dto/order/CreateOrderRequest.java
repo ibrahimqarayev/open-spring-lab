c123c2da6a20b756f043b49b5bf37d776ac3ac1c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-01-10 14:48:26 +0400

    feat: add CreateOrderRequest

---
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/dto/order/CreateOrderRequest.java
@@ -0,0 +1,13 @@
+package com.openspringlab.ecommerce.dto.order;
+
+import com.openspringlab.ecommerce.dto.order.item.CreateOrderItemRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class CreateOrderRequest {
+    private List<CreateOrderItemRequest> items;
+}
