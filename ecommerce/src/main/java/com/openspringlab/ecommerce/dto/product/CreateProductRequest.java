88c367dc610d4c7cb2d3963e9df1e893842af51c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-01-10 14:55:12 +0400

    feat: update CreateProductRequest

--- ecommerce/src/main/java/com/openspringlab/ecommerce/dto/product/CreateProductRequest.java
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/dto/product/CreateProductRequest.java
@@ -1,31 +1,17 @@
-package com.openspringlab.ecommerce.dto.request;
+package com.openspringlab.ecommerce.dto.product;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class CreateProductRequest {
-
     private String name;
-
     private String description;
-
     private BigDecimal price;
-
     private String sku;
-
     private Integer stockQuantity;
-
-    private Boolean active;
-
     private Long categoryId;
-
 }
