94be8abbbd797a0d661e679a81e073a3fbf44d41
unknown <mustafaraza03898@gmail.com>
2026-01-11 22:04:31 +0530

    feat:add all required mapping interfaces.

--- ecommerce/src/main/java/com/openspringlab/ecommerce/mapper/OrderMapper.java
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/mapper/OrderMapper.java
@@ -1,4 +1,14 @@
 package com.openspringlab.ecommerce.mapper;
 
+import com.openspringlab.ecommerce.dto.order.OrderResponse;
+import com.openspringlab.ecommerce.model.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = { OrderItemMapper.class })
 public interface OrderMapper {
+
+    @Mapping(source = "orderItems", target = "items")
+    OrderResponse toDto(Order order);
 }
